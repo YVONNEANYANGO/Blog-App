7cfe10840195ffa13c2cd5f24898c4c6e2278443
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-23 18:28:13 +0300

    add forms where users can fill in their email address and passwords

--- manage.py
+++ manage.py
@@ -1,30 +1,19 @@
-<div class="navbar navbar-inverse" role="navigation">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">ADVENTURE</a>
-        </div>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="/home">HOME</a></li>
-                <li><a href="/about">ABOUT</a></li>
-                <li><a href="/latest">LATEST INFO</a></li>
-                <li><a href="/contacts">CONTACTS</a></li>
-            </ul>
-        </div>
-        
-        <ul class="nav navbar-nav navbar-right">
-            {% if current_user.is_authenticated %}
-            <li style="color:cream;padding:15px;">{{current_user.username}}</li>
-            <li><a href="{{url_for('auth.logout')}}">Sign out</a></li>
-            {% else %}
-            <li><a href="{{url_for('auth.login')}}">Sign in</a></li>
-            {%endif%}
-        </ul>
-    </div>
-</div>
+from app import create_app
+from flask_script import Manager, Server
+from app.models import User,db,Pitch,Comment
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app('production')
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+manager.add_command('server', Server)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Pitch = Pitch, Comment = Comment )
+
+if __name__ == '__main__':
+    manager.run()
+           
